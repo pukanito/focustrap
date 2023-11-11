842d8b1e3bac0c369ae37b40372b58e7ebab6a6f
Pukanito Forager <pukanito@gmail.com>
2023-11-11 12:19:06 +0100

    Fix NG0100

--- test-focusable/src/app/app.component.ts
+++ test-focusable/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Focusable } from '@pukanito/focusable';
 
 @Component({
@@ -8,12 +8,15 @@ import { Focusable } from '@pukanito/focusable';
 })
 export class AppComponent implements AfterViewInit {
   focusOk: boolean = false;
+
+  @ViewChild('focusableContainer') private focusableContainer!: ElementRef;
+
   ngAfterViewInit(): void {
-    const focus = new Focusable(
-      document.querySelector('.focus-container') as HTMLElement
-    );
-    this.focusOk =
-      focus.firstFocusable!.getAttribute('data-focus') === 'first-element' &&
-      focus.lastFocusable!.getAttribute('data-focus') === 'last-element';
+    setTimeout(() => {
+      const focus = new Focusable(this.focusableContainer.nativeElement);
+      this.focusOk =
+        focus.firstFocusable!.getAttribute('data-focus') === 'first-element' &&
+        focus.lastFocusable!.getAttribute('data-focus') === 'last-element';
+    }, 0);
   }
 }
