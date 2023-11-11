fe22637d54e0e5c37b8a15d7d508aa7977450e45
Pukanito Forager <pukanito@gmail.com>
2023-11-11 12:20:09 +0100

    Add test-focustrap application

---
+++ test-focustrap/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FocusTrap } from '@pukanito/focustrap';
+
+@Component({
+  selector: 'test-focustrap',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  focusTrapSwitch = new FormControl(false);
+
+  @ViewChild('focusTrapContainer') private container!: ElementRef;
+
+  private focusTrap: FocusTrap | null = null;
+
+  ngOnInit(): void {
+    this.focusTrapSwitch.valueChanges.subscribe((value) => {
+      if (value) {
+        this.focusTrap = new FocusTrap(this.container.nativeElement);
+      } else {
+        this.focusTrap?.off();
+        this.focusTrap = null;
+      }
+    });
+  }
+}
